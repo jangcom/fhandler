c97ee5c62ece686cbb98914f98e56337695298fc
jang <jangj@korea.ac.kr>
2020-07-05 13:00:14 +0900

    Correct .pod

--- trial.bat
+++ trial.bat
@@ -1,9 +1,9 @@
 @echo off
 rem File by file
-python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _inc --num_space=4
-python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _dec --num_space=-4
-python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _inc_dec --num_space=4
-python fhandler.py ./samples/proglangs_inc_dec.tex --func adj_leading_spaces --inplace --num_space=-4
+python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _inc --num_spaces=4
+python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _dec --num_spaces=-4
+python fhandler.py ./samples/proglangs.tex --func adj_leading_spaces --flag _inc_dec --num_spaces=4
+python fhandler.py ./samples/proglangs_inc_dec.tex --func adj_leading_spaces --inplace --num_spaces=-4
 
 rem Multiple files at once
 echo on
